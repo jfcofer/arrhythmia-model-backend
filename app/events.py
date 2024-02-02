c9c8005861d501eadec6f954e52df77f2818870f
JFcoFernandez <juaffern@espol.edu.ec>
2024-02-01 22:05:39 -0500

    Added events

--- app/events.py
+++ app/events.py
@@ -1,6 +1,16 @@
+from app.services.arrhythmia_service import ArrhythmiaService
 from app.socketio import socketio
+from flask_socketio import emit
 
+arrhythmia_service = ArrhythmiaService
 
 @socketio.on("connect")
 def handle_connect():
     print("Client connected")
+
+
+@socketio.on("heartbeat_input")
+def handle_new_data(data):
+    heartbeat_data = data["heartbeat_data"]
+    prediction = arrhythmia_service.predict_arrhythmia(heartbeat_data)
+    emit("heartbeat_output", {"prediction": prediction, "heartbeat_data": heartbeat_data})    
